0337a34bae632eb8ac0e133f512b3e95f60dbddd
Danizold34 <79314048+Danizold34@users.noreply.github.com>
2021-03-11 17:42:42 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 module.exports = function reverse (n) {
    let arr = new Array(n);
-   return arr.reverse().join("");
+   return Number(arr.reverse().join(""));
     //let num = Number(arr)
     
 }
